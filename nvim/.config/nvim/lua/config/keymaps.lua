8adb059ce5038a7943acfb806f232c1c190fab19
YordanM-IL <yordan@infinitelambda.com>
2025-02-08 20:26:56 +0200

    Added venv-selector

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local wk = require("which-key")
+
+wk.register({
+  p = {
+    v = { ":VenvSelect<CR>", "Select Virtual Environment" },
+    c = { ":VenvSelectCached<CR>", "Use Cached Virtual Environment" },
+  },
+}, { prefix = "<leader>" })
