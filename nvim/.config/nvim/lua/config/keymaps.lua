97c8e0e595643cc8e7e06ffeb5f504e3a35b058d
YordanM-IL <yordan@infinitelambda.com>
2025-02-11 17:03:13 +0200

    Updated the keymaps file with the new syntax

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,30 +3,58 @@
 -- Add any additional keymaps here
 
 -- keymaps.lua
-local wk = require("which-key")
-
-wk.register({
+local keymaps = {
   -- Virtual Environment commands
-  p = {
-    v = { ":VenvSelect<CR>", "Select Virtual Environment" },
-    c = { ":VenvSelectCached<CR>", "Use Cached Virtual Environment" },
-  },
+  ["<leader>pv"] = { ":VenvSelect<CR>", "Select Virtual Environment" },
+  ["<leader>pc"] = { ":VenvSelectCached<CR>", "Use Cached Virtual Environment" },
 
   -- Terminal key mappings
-  t = {
-    h = { ":split | terminal<CR>", "Open Horizontal Terminal" },
-    v = { ":vsplit | terminal<CR>", "Open Vertical Terminal" },
-  },
+  ["<leader>th"] = { ":split | terminal<CR>", "Open Horizontal Terminal" },
+  ["<leader>tv"] = { ":vsplit | terminal<CR>", "Open Vertical Terminal" },
 
   -- Debugging key mappings
-  d = {
-    b = { ":lua require'dap'.toggle_breakpoint()<CR>", "Toggle Breakpoint" },
-    c = { ":lua require'dap'.continue()<CR>", "Continue Debugging" },
-    s = { ":lua require'dap'.step_into()<CR>", "Step Into" },
-    o = { ":lua require'dap'.step_over()<CR>", "Step Over" },
-    q = { ":lua require'dap'.close()<CR>", "Close Debugger" },
+  ["<leader>db"] = {
+    function()
+      require("dap").toggle_breakpoint()
+    end,
+    "Toggle Breakpoint",
   },
-}, {
-  prefix = "<leader>", -- The leader key (space)
-  mode = "n", -- Normal mode (you can adjust this for other modes as well)
-})
+  ["<leader>dc"] = {
+    function()
+      require("dap").continue()
+    end,
+    "Continue Debugging",
+  },
+  ["<leader>ds"] = {
+    function()
+      require("dap").step_into()
+    end,
+    "Step Into",
+  },
+  ["<leader>do"] = {
+    function()
+      require("dap").step_over()
+    end,
+    "Step Over",
+  },
+  ["<leader>dq"] = {
+    function()
+      require("dap").close()
+    end,
+    "Close Debugger",
+  },
+
+  -- Telekasten keymaps (Second Brain)
+  ["<leader>zp"] = { ":Telekasten panel<CR>", "Open Telekasten panel" },
+  ["<leader>zf"] = { ":Telekasten find_notes<CR>", "Find Notes" },
+  ["<leader>zg"] = { ":Telekasten live_grep_notes<CR>", "Search Notes" },
+  ["<leader>zn"] = { ":Telekasten new_note<CR>", "New Note" },
+  ["<leader>zd"] = { ":Telekasten goto_today<CR>", "Today's Note" },
+  ["<leader>zj"] = { ":Telekasten follow_link<CR>", "Follow Note Link" },
+  ["<leader>zb"] = { ":Telekasten show_backlinks<CR>", "Show Backlinks" },
+}
+
+-- Apply the new keymap format
+for key, mapping in pairs(keymaps) do
+  vim.keymap.set("n", key, mapping[1], { desc = mapping[2] })
+end
