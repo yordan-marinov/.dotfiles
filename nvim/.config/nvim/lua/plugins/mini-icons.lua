df1bf75c09fc5f72584312251f72e89740d038e9
YordanM-IL <yordan@infinitelambda.com>
2025-11-04 13:05:21 +0200

    Added mini-icons plugin

---
+++ nvim/.config/nvim/lua/plugins/mini-icons.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-mini/mini.icons",
+  lazy = true,
+  opts = {
+    file = {
+      [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+      ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+    },
+    filetype = {
+      dotenv = { glyph = "", hl = "MiniIconsYellow" },
+    },
+  },
+  init = function()
+    -- Override nvim-web-devicons to use mini.icons
+    package.preload["nvim-web-devicons"] = function()
+      require("mini.icons").mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
+  end,
+  config = function(_, opts)
+    require("mini.icons").setup(opts)
+  end,
+}
